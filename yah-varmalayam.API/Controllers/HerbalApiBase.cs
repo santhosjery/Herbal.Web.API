0a724a93cb61a964db1f147e561ddf570925f4c8
Santhosh Nallasamy <santhosh.nallasamy@Impigertech.com>
2020-10-04 21:11:03 +0530

    Added post for User.

--- yah-varmalayam.API/Controllers/HerbalApiBase.cs
+++ yah-varmalayam.API/Controllers/HerbalApiBase.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using yah_varmalayam.API.Models;
 
 namespace yah_varmalayam.API.Controllers
 {
     public class HerbalApiControllerBase : ApiController
     {
-
+        protected HerbalEntities dbContext = new HerbalEntities();
+        
     }
 }
--- yah-varmalayam.API/Controllers/User.cs
+++ yah-varmalayam.API/Controllers/User.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using yah_varmalayam.API.Models;
 
 namespace yah_varmalayam.API.Controllers
 {
@@ -12,6 +13,19 @@ namespace yah_varmalayam.API.Controllers
         public bool Get()
         {
             return true;
+        }
+        public bool Post(AppUser appUser)
+        {
+            try
+            {
+                dbContext.AppUsers.Add(appUser);
+                dbContext.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
